fa0ec469ccf34e819f8d7bc3f5b6d2373bd861b6
Kenny Deckers <deckers.kenny@gmail.com>
2020-12-12 23:44:45 +0100

    Housekeeping :)

--- .github/workflows/check_upstream_versions.py
+++ .github/workflows/check_upstream_versions.py
@@ -21,8 +21,6 @@ try:
         os.environ['GITHUB_REPOSITORY'], command='git --work-tree=/opt/cloudplow rev-parse HEAD', auto_remove=True, entrypoint='').decode('UTF-8').strip()
     old_versions['alpine-cloudplow-rclone_commit_ref'] = client.images.get(
         os.environ['GITHUB_REPOSITORY']).labels['org.label-schema.vcs-ref']
-    old_versions['s6_release'] = client.containers.run(
-        os.environ['GITHUB_REPOSITORY'], command='cat /etc/S6_RELEASE', auto_remove=True, entrypoint='').decode('UTF-8').strip()
     print("Detected image versions:\n" + json.dumps(old_versions, indent=2))
 except docker.errors.ContainerError as e:
     print(e)
@@ -31,8 +29,6 @@ current_versions['rclone_release_name'] = requests.get(
     'https://api.github.com/repos/rclone/rclone/releases/latest').json()["tag_name"]
 current_versions['cloudplow_commit_ref'] = requests.get(
     'https://api.github.com/repos/l3uddz/cloudplow/commits/master').json()["sha"]
-current_versions['alpine-cloudplow-rclone_commit_ref'] = Repo(
-    '.').commit('HEAD').hexsha
 current_versions['s6_release'] = requests.get(
     'https://api.github.com/repos/just-containers/s6-overlay/releases/latest').json()["tag_name"]
 
